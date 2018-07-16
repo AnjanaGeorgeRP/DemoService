9da24b78694c9f42779388eae3aa8ef12e031ba9
AnjanaGeorge <16004097@myrp.edu.sg>
2018-07-16 13:10:49 +0800

    Changed service to access location services

--- app/src/main/java/com/myapplicationdev/android/demoservice/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoservice/MainActivity.java
@@ -1,13 +1,68 @@
 package com.myapplicationdev.android.demoservice;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnStart,btnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart=findViewById(R.id.btnStart);
+        btnStop=findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkPermission()) {
+                    Intent i = new Intent(MainActivity.this, MyService.class);
+                    startService(i);
+                }else {
+                    Toast.makeText(MainActivity.this, "Permission not granted to retrieve location info", Toast.LENGTH_LONG).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                }
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPermission()) {
+                    Intent i = new Intent(MainActivity.this, MyService.class);
+                    stopService(i);
+                }else {
+                    Toast.makeText(MainActivity.this, "Permission not granted to retrieve location info", Toast.LENGTH_LONG).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                }
+            }
+        });
+
     }
+
+    public boolean checkPermission() {
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
